b59505ea1bc5bb9befa58e9847c6b44110aa147c
venusn99 <venusn99@gmail.com>
2023-11-06 10:08:16 +0530

    Update TEP-to-TEP_Ping_Paramiko.py
    added exception: modulus by 0 error

--- TEP-to-TEP_Ping_Paramiko.py
+++ TEP-to-TEP_Ping_Paramiko.py
@@ -7,6 +7,7 @@ import sys
 import threading
 import logging
 import datetime
+#import pyfiglet
 
 start_time = time.time() #getting start time
 
@@ -16,29 +17,48 @@ T_height=os.get_terminal_size().lines # extracting local machines terminal heigh
 src_tep_ips = []
 dst_tep_ips = []
 log_file = datetime.datetime.now().strftime('tep2tep_ping_%d%m%y_%H_%M_%S.log') #log file name to send the output to logfile from functions.
+# Requirements and pre-checks function
 def main():
-    edge_ip = input("Enter an Edge IP address,(no plural)\n") #enter the Edge IP, only provide 1 IP address
-    username = "admin" # change the username accordingly
-    delay_print="Username \"admin\" is considered as default."
-    for c in delay_print:
-        sys.stdout.write(c)
-        sys.stdout.flush()
-        time.sleep(0.03)
-    print()
-    #password = input("Enter the SSH password for Edge") #provide the password here
-    password = getpass.getpass("Enter the SSH password for Edge\n") #hiding the password entry
-    port = 22
-    timeout_seconds = 120 # if you see get a Socket timeout error increase the value to the approx time required to execute the commands.
-    ssh_connection = ssh_connect(edge_ip, port, username, password,timeout_seconds)
-    src_tep_thread = threading.Thread(target=get_src_tep,kwargs={'ssh_connection':ssh_connection})
-    dst_tep_thread = threading.Thread(target=get_dst_tep,kwargs={'ssh_connection':ssh_connection})
-    src_tep_thread.start()
-    dst_tep_thread.start()
-    src_tep_thread.join()
-    dst_tep_thread.join()
-    print("-------------------------\n",src_tep_ips,dst_tep_ips)
-    tep_to_tep_ping(ssh_connection, src_tep_ips, dst_tep_ips)
-    ssh_close(ssh_connection)
+    while True:
+        try:
+            edge_ip = input("Enter an Edge IP address,(no plural)\n") #enter the Edge IP, only provide 1 IP address
+            username = "admin" # change the username accordingly
+            delay_print="Username \"admin\" is considered as default."
+            for c in delay_print:
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(0.03)
+            print()
+            #password = input("Enter the SSH password for Edge") #provide the password here
+            password = getpass.getpass("Enter the SSH password for Edge\n") #hiding the password entry
+            port = 22
+            timeout_seconds = 120 # if you see get a Socket timeout error increase the value to the approx time required to execute the commands.
+            delay_print="Warning ⚠️ pre-requisites: Atlease 1 VM/Test VM has to be present on each host connecting to NSX Overlay Segments\nhint: https://github.com/venusn99/pyvmomi-instant-VM-clone\nStarting the execution in:\n"
+            for c in delay_print:
+                sys.stdout.write(c)
+                time.sleep(0.03)
+                sys.stdout.flush()
+            for counter in reversed(range(5)):
+                print(counter)
+                time.sleep(1)
+                counter -= counter
+            ssh_connection = ssh_connect(edge_ip, port, username, password,timeout_seconds)
+            src_tep_thread = threading.Thread(target=get_src_tep,kwargs={'ssh_connection':ssh_connection})
+            dst_tep_thread = threading.Thread(target=get_dst_tep,kwargs={'ssh_connection':ssh_connection})
+            src_tep_thread.start()
+            dst_tep_thread.start()
+            src_tep_thread.join()
+            dst_tep_thread.join()
+            print("-------------------------\n",src_tep_ips,dst_tep_ips)
+            tep_to_tep_ping(ssh_connection, src_tep_ips, dst_tep_ips)
+            ssh_close(ssh_connection)
+            if input("\n\n\n\n---------> Do you want to continue? (Hit 'N' to exit) <----------\n\n\n") == 'N':
+                print("\n\n\n\t\t\tGood Bye 🙋🏽‍♂️ ", "\n\n\n")
+                break
+        except KeyboardInterrupt:
+            if ssh_connection:
+                ssh_close(ssh_connection)
+                break
 # The function will SSH into the edge host
 def ssh_connect(ip, port, username, password,timeout_ssh):
     try:
@@ -46,10 +66,11 @@ def ssh_connect(ip, port, username, password,timeout_ssh):
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh_client.connect(ip, port, username, password,timeout=timeout_ssh)
         print(f"SSH connected to {ip}")
+        logging_func(f"\n\nSSH connected to {ip}\n")
         return ssh_client # returning the ssh connection stat  ssh_connection variable
     except Exception as e:
         print(f"Exception occured: {str(e)}")
-
+# function to retrive source TEP IPs from Edge Nodes
 def get_src_tep(ssh_connection):
     try:
         output_buffer = ""
@@ -78,7 +99,7 @@ def get_src_tep(ssh_connection):
     finally:
         ssh_shell.close()
         print("Interactive ssh shell closed(get_src_tep)")
-
+# function to retrive Destination TEP IPs from Edge Nodes
 def get_dst_tep(ssh_connection):
     try:
         output_buffer = ""
@@ -110,6 +131,7 @@ def get_dst_tep(ssh_connection):
     finally:
         ssh_shell.close()
         print("Interactive SSH shell closed (get_dst_tep)")
+# function to TEP-TEP ping 
 def tep_to_tep_ping(ssh_connection, src_ip, dst_ip):
     try:
         output = ""
@@ -159,6 +181,7 @@ def tep_to_tep_ping(ssh_connection, src_ip, dst_ip):
     finally:
         ssh_shell.close()
         print("Interactive SSH shell closed (tep_to_tep_ping)")
+# Summarizing the TEP-TEP function output below
 def summarize_TEP_ping(f_cont):
     logme=''
     #print(f_cont)
@@ -171,15 +194,21 @@ def summarize_TEP_ping(f_cont):
         #print(f"{i+1} {match_header[i]}")
         logme += f"\n{i+1} {match_header[i]}\n"
         #print(match_output[i]) #this will print whole "sent , received and loss" from ping output
-        if int(match_output[i][0]) % int(match_output[i][1]) > 0.0:
-            #print("     Alert: Packet loss found below")
-            logme += "     Alert: Packet loss found below"
+        sent_pkt = int(match_output[i][0])
+        received_pkt = int(match_output[i][1])
+        if received_pkt > 0:
+            if int(match_output[i][0]) % int(match_output[i][1]) > 0.0:
+                logme += "     Alert: Packet loss found below\n"
+        else:
+            logme += "     Alert: No packets received\n"
         #print(f"    Sent {match_output[i][0]}, Received {match_output[i][1]}, Lost %: {match_output[i][2]} \n") #replace of this is "logme variable"
         logme += f"    Sent {match_output[i][0]}, Received {match_output[i][1]}, Lost %: {match_output[i][2]} \n"
     print(logme)
     logging_func(logme) #sending cont of log variable to logger function
+#Universal Logging Function
 def logging_func(log_cont):
-        logging.basicConfig(level=logging.DEBUG, filename=log_file, filemode="a+",format="%(asctime)-15s %(levelname)-8s %(message)s")
+        #logging.basicConfig(level=logging.DEBUG, filename=log_file, filemode="a+",format="%(asctime)-15s %(levelname)-8s %(message)s")
+        logging.basicConfig(level=logging.DEBUG, filename=log_file, filemode="a+")
         logging.info(log_cont)
 # The function will close the SSH of edge host
 def ssh_close(ssh_connection):
