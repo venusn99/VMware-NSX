35233ca818f0a1d4a9727290a40e638fd629b1dc
venusnvenu <venus.vmware@gmail.com>
2024-11-14 16:03:55 +0530

    takes comma seperated values for source and destination TEPs

--- nsx-api-to-fetch-tep.py
+++ nsx-api-to-fetch-tep.py
@@ -3,4 +3,4 @@ import getpass
 # requests.get(url, params={key: value}, args)
 nsx_manager_ip= ''
 password= getpass.getpass(f"Enter the password for NSX Manager{nsx_manager_ip}")
-r = requests.get(url=https://{nsx_manager_ip}/)
+r = requests.get(url="https://{nsx_manager_ip}/")
